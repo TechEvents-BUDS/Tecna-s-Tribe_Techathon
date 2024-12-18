3ab39c2aba7055e498b4c2bc7328f02e96ff426b
row-huh <rohaabropathan@gmail.com>
2024-12-18 11:06:50 +0500

    Uploaded dataset

--- model/genai.py
+++ model/genai.py
@@ -11,3 +11,7 @@ genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Explain how AI works")
 print(response.text)
+
+
+class AI():
+    ...
---
+++ model/utility.py
@@ -0,0 +1,3 @@
+
+# edit later
+SYSTEM_PROMPT = """Your name is Neutral. You are a tool that assists in making good hiring decisions."""
